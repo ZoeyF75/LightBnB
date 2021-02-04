6636ea916086c1e635892fa4547cc8331b6b764c
Zoey Farago <zf.zoodle@gmail.com>
2021-02-04 04:16:38 +0000

    LightBnB general SQL queries

---
+++ 1_queries/user_login.sql
@@ -0,0 +1,4 @@
+-- The email example given in the assigment was overwritten because I already implemented a user with the given id
+SELECT id, name, email, password
+FROM users
+WHERE email = 'michaelgray@mail.com';
